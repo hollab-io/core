002d78c67aab1f013a75719612509f0bc20a3a8c
danil-sergeev <danilsergeev.pt@protonmail.com>
2026-04-04 10:41:17 +0200

    feat: ens + holacracy contracts

--- packages/contracts/src/libraries/HolacracyTypes.sol
+++ packages/contracts/src/libraries/HolacracyTypes.sol
@@ -119,6 +119,10 @@ library HolacracyTypes {
     address accessManager;
     uint256 anchorCircleId;
     uint256 createdAt;
+    // On-chain governance
+    address governor;
+    address token;
+    address timelock;
   }
 
   /// @notice An objection raised against a proposal
